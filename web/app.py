ec5e70722d9a49c86a06add592d604508b58c049
Beato Bongco <beatobongco@gmail.com>
2015-02-10 22:27:33 +0800

    Fix app debug updating, add README.md

--- web/app.py
+++ web/app.py
@@ -3,7 +3,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-  return "Hello World!"
+  app.logger.debug("This is a debug message.")
+  return "Hello !!!@!"
 
 if __name__ == "__main__":
-    app.run()
+  app.run(host='0.0.0.0', port=1337, debug=True)
